7ef3f8a5188cf1b4c7c970a393256d7b7504c687
Pravinsk24 <kamarajs756@gmail.com>
2025-03-20 09:53:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,7 +107,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://coloured-molli-movieblaster-1cd04d26.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
